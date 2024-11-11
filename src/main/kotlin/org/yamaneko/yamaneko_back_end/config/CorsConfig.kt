aa072bd7fde14c69343e36efc7a7a87bd9051c49
Miryoku <dragomore2804@gmail.com>
2024-11-11 05:59:26 +0200

    Add new cors

--- src/main/kotlin/org/yamaneko/yamaneko_back_end/config/CorsConfig.kt
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/config/CorsConfig.kt
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig: WebMvcConfigurer {
     override fun addCorsMappings( registry: CorsRegistry ) {
         registry.addMapping("/api/**")
-            .allowedOrigins("https://yamaneko.isn.one", "http://localhost:5173", "http://localhost:8080")
+            .allowedOrigins("https://yamaneko.isn.one", "http://localhost:5173", "http://localhost:8080", "https://admin.yamaneko.isn.one/")
             .allowedMethods("GET", "POST", "PUT", "DELETE" )
             .allowedHeaders("*")
             .allowCredentials( true )
