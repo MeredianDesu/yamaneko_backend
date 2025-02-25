c0682ca8c8fab98c8f75f94c51a8fa37bf8fb8e4
dragomore <dragomore2804@gmail.com>
2025-02-25 22:34:19 +0200

    Dateformat fix

--- src/main/kotlin/org/yamaneko/yamaneko_back_end/repository/UserRefreshTokensRepository.kt
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/repository/UserRefreshTokensRepository.kt
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param
 import org.yamaneko.yamaneko_back_end.entity.RefreshToken
 
 interface UserRefreshTokensRepository: JpaRepository<RefreshToken, Long>{
-    @Query( "FROM RefreshToken a WHERE a.user.id = :userId AND TO_TIMESTAMP( a.expiresAt, 'dd-MM-yyyy HH:mm:ss' ) > CURRENT_TIMESTAMP" )
+    @Query( "FROM RefreshToken a WHERE a.user.id = :userId AND TO_TIMESTAMP( a.expiresAt, 'dd-MM-yyyy HH24:mi:ss' ) > CURRENT_TIMESTAMP" )
     fun findByUser(@Param( "userId" ) userId: Long ): RefreshToken?
 
 //    @Modifying
