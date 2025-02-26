59bfa656d62dce3db9716028ddb49b33651f845e
dragomore <dragomore2804@gmail.com>
2025-02-26 22:14:18 +0200

    YAMR-160 <Minor swagger refactor>

--- src/main/kotlin/org/yamaneko/yamaneko_back_end/api/controllers/private_api/NewsController.kt
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/api/controllers/private_api/NewsController.kt
@@ -1,6 +1,7 @@
 package org.yamaneko.yamaneko_back_end.api.controllers.private_api
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.yamaneko.yamaneko_back_end.dto.news.NewsDTO
 import org.yamaneko.yamaneko_back_end.service.news.NewsService
 
+@Tag(name = "{ v1 } News API")
 @RestController
 @RequestMapping("api/news/v1")
 class NewsController( @Autowired private val newsService: NewsService ) {
--- src/main/kotlin/org/yamaneko/yamaneko_back_end/api/controllers/private_api/UserController.kt
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/api/controllers/private_api/UserController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -17,6 +18,7 @@ import org.yamaneko.yamaneko_back_end.repository.UserRepository
 import org.yamaneko.yamaneko_back_end.service.user.UserService
 import org.yamaneko.yamaneko_back_end.utils.JwtUtil
 
+@Tag(name = "{ v1 } Users API")
 @RestController
 @RequestMapping( "/api/users/v1" )
 @Validated
