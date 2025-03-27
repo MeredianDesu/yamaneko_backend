6f4b13e5a165aa578ae80bd3845111ecf4919418
dragomore <dragomore2804@gmail.com>
2025-03-27 18:21:58 +0200

    Change JWT extraction & SecurityConfig & Refactor user entity

---
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/customExceptions/JwtSignatureException.kt
@@ -0,0 +1,3 @@
+package org.yamaneko.yamaneko_back_end.customExceptions
+
+class JwtSignatureException(message: String): RuntimeException(message) {}
---
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/entity/Role.kt
@@ -0,0 +1,5 @@
+package org.yamaneko.yamaneko_back_end.entity
+
+enum class Role {
+  ROLE_USER, ROLE_ADMIN
+}
