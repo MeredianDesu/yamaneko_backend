35b2d4bb2b43bab78d04a65fc85e5db8079c30f0
dragomore <dragomore2804@gmail.com>
2025-04-23 23:16:21 +0300

    Cancel /user @Depricated; Add ACL

--- src/main/kotlin/org/yamaneko/yamaneko_back_end/service/s3/S3Service.kt
+++ src/main/kotlin/org/yamaneko/yamaneko_back_end/service/s3/S3Service.kt
@@ -9,6 +9,7 @@ import org.yamaneko.yamaneko_back_end.service.episode.EpisodeService
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
@@ -74,8 +75,8 @@ class S3Service {
     S3Presigner.builder().region(Region.of(s3Config.region))
       .credentialsProvider(StaticCredentialsProvider.create(credentials))
       .endpointOverride(URI.create(s3Config.endpoint)).build().use { presigner ->
-        val putObjectRequest =
-          PutObjectRequest.builder().bucket(s3Config.bucketName).key("users/$username/$filename").build()
+        val putObjectRequest = PutObjectRequest.builder().bucket(s3Config.bucketName).key("users/$username/$filename")
+          .acl(ObjectCannedACL.PUBLIC_READ).build()
         
         val presignRequest =
           PutObjectPresignRequest.builder().signatureDuration(Duration.ofSeconds(10)).putObjectRequest(putObjectRequest)
